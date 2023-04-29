3b979740482c1f59d0025f4998e9c53cfd6d409f
Abdulrahman Tabaza <84684514+a-tabaza@users.noreply.github.com>
2023-04-29 15:47:56 +0300

    fix sizing with flatten

--- app.py
+++ app.py
@@ -39,9 +39,9 @@ uploaded_file = st.file_uploader("Choose a file", type=['png', 'jpg', 'jpeg'])
 
 if uploaded_file is not None:
     image, bands = image_loader(uploaded_file)
-    data = flatten_image(image)
     h, w = image.size
     image = image.resize((int(h/3),int(w/3)))
+    data = flatten_image(image)
     
 class Clusterer():
     def __init__(self, df, colors=8):
